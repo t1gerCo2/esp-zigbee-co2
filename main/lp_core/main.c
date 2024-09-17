8dd78e00fc63234659d77c00e27982a4d8bba3a3
t1gerCo2 <andreaskalen42@gmail.com>
2024-09-17 09:18:51 +0200

    Update main.c

--- main/lp_core/main.c
+++ main/lp_core/main.c
@@ -7,7 +7,7 @@
 
 #include "../SensirionI2CScd4x.h"
 
-#define LP_UART_PORT_NUM    LP_UART_NUM_0
+/* #define LP_UART_PORT_NUM    LP_UART_NUM_0 */
 
 #define LP_I2C_TRANS_TIMEOUT_CYCLES 5000
 #define LP_I2C_TRANS_WAIT_FOREVER   -1
